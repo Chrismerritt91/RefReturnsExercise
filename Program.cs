ab8fbdccac8abd9e47eb45c2c1db0e74759d3f71
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-03 09:27:17 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿class Student {
+
+    //public field
+    public int grade = 2;
+
+    //public method
+    public void PrintGrade() {
+
+        Console.WriteLine("Grade: " + grade);
+    }
+
+    //public method with ref return
+    public ref int DoWork() {
+
+        // return refeerence of grade field
+        return ref grade;
+    }
+
+}
+
+
+class Program {
+
+    static void Main() {
+
+        //creating object of student
+        Student s = new Student();
+
+        //Call print grade
+        s.PrintGrade();
+
+        //call do work method
+        ref int g = ref s.DoWork();
+
+        //update the value of ref return
+        g = 5;
+
+        //Call print grade after updating the value of ref return
+        s.PrintGrade();
+
+        System.Console.ReadKey();
+
+    }
+
+}
